f1431dbfee3de1fb13d7d8a3ba296da8a0b092fd
asukayagi <asuka@thinkpad.asuka>
2018-09-20 23:18:21 +0900

    add go-slim

---
+++ go-slim/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mattn/go-slim"
+	"log"
+	"os"
+)
+
+func main() {
+	tmpls := []string{
+		"template/good.slim",
+		"template/good2.slim",
+		"template/bad.slim",
+		"template/bad2.slim",
+		"template/bad3.slim",
+	}
+	for _, v := range tmpls {
+		fmt.Printf("\n--- %s ---\n", v)
+		tmpl, err := slim.ParseFile(v)
+		if err != nil {
+			logErr(v, err)
+			continue
+		}
+		err = tmpl.Execute(os.Stdout, slim.Values{
+			"nums": []int{1, 2, 3},
+		})
+		if err != nil {
+			logErr(v, err)
+		}
+	}
+}
+
+func logErr(s string, e error) {
+	log.Fatalf("%s:%v", s, e)
+}
